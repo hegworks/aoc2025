5731636f6dacb396902babdf47951f5592492cea
Hesam Ghadimi <hess.gha@gmail.com>
2025-12-01 15:22:39 +0100

    feat : day1 solve

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -10,7 +10,49 @@ int main()
     FileReader fr{};
     fr.LoadFile("input.txt");
 
-    std::cout << fr.Cin();
+    int dial{50};
+    int zero_counter{0};
+
+    const int total_line_count = fr.LineCount();
+    for (int line_idx = 0; line_idx < total_line_count; ++line_idx)
+    {
+        std::string input_str = fr.Cin();
+        std::string first_char_str{input_str.at(0)};
+        std::string input_dial_change_str = input_str.substr(1);
+        int input_dial_change = std::stoi(input_dial_change_str);
+
+        input_dial_change = input_dial_change % 100;
+
+        if (first_char_str == "L")
+        {
+            dial -= input_dial_change;
+        }
+        else
+        {
+            dial += input_dial_change;
+        }
+
+        if (dial > 99)
+        {
+            dial = dial - 100;
+        }
+        if (dial < 0)
+        {
+            dial = dial + 100;
+        }
+
+        if (dial == 0)
+        {
+            zero_counter++;
+        }
+    }
+
+    printf("\n");
+    printf("------------");
+    printf("\n");
+    printf("zeroes: %d", zero_counter);
+    printf("\n");
+    printf("dial: %d", dial);
 
     printf("\n");
 }
--- shared_lib/include/FileReader.h
+++ shared_lib/include/FileReader.h
@@ -8,31 +8,30 @@
 class FileReader
 {
 public:
-	void LoadFile(std::string path)
-	{
-		loadedFileLines.clear();
-		std::ifstream file(path);
-		if(file.is_open())
-		{
-			std::string line;
-			while(std::getline(file, line))
-			{
-				loadedFileLines.push_back(line);
-			}
-			file.close();
-		}
-		else
-		{
-			std::cout << "ERROR::Unable to open file " << path;
-		}
-	}
+    void LoadFile(const std::string& path)
+    {
+        m_loaded_file_lines.clear();
+        std::ifstream file(path);
+        if (file.is_open())
+        {
+            std::string line;
+            while (std::getline(file, line))
+            {
+                m_loaded_file_lines.push_back(line);
+            }
+            file.close();
+        }
+        else
+        {
+            std::cout << "ERROR::Unable to open file " << path;
+        }
+    }
 
-	std::string Cin()
-	{
-		return loadedFileLines[readLine++];
-	}
+    std::string Cin() { return m_loaded_file_lines[line_num++]; }
+
+    int LineCount() const { return static_cast<int>(m_loaded_file_lines.size()); }
 
 private:
-	std::vector<std::string> loadedFileLines;
-	int readLine = 0;
+    std::vector<std::string> m_loaded_file_lines;
+    int line_num = 0;
 };
